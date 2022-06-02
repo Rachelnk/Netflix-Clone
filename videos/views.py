331d2254282a528ccaca0b6702a6cb131fe41bca
Rachel Kiarie <rachelnnk94@gmail.com>
2022-06-02 16:09:56 +0300

    Get now playing movies and top rated tv shows.

--- videos/views.py
+++ videos/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from Netflix.settings import TMDB_API_KEY
+import requests
 
 # Create your views here.
+
+def view_all(request):
+  now_playing_movies_request = requests.get("https://api.themoviedb.org/3/movie/now_playing?api_key=" + TMDB_API_KEY)
+  now_playing_movies_result = now_playing_movies_request.json()
+  now_playing_movies = now_playing_movies_result['results']
+
+  top_rated_tv_request = requests.get("https://api.themoviedb.org/3/tv/top_rated?api_key=" + TMDB_API_KEY)
+  top_rated_tv_result = top_rated_tv_request.json()
+  top_rated_tv = top_rated_tv_result['results']
+
+  return render(request,'all-movies-tvs.html', {'now_playing_movies': now_playing_movies,})
+
