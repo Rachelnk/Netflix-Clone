912daac47394895bf7ca89aff14a5923d4918108
Rachel Kiarie <rachelnnk94@gmail.com>
2022-06-02 16:24:26 +0300

    Get popular movies and tv, upcoming movies.

--- videos/views.py
+++ videos/views.py
@@ -5,13 +5,39 @@ import requests
 # Create your views here.
 
 def view_all(request):
+
+  # get now playing movies
   now_playing_movies_request = requests.get("https://api.themoviedb.org/3/movie/now_playing?api_key=" + TMDB_API_KEY)
   now_playing_movies_result = now_playing_movies_request.json()
   now_playing_movies = now_playing_movies_result['results']
 
+  # get popular tv shows
+  popular_tv_shows_request = requests.get("https://api.themoviedb.org/3/tv/popular_tv?api_key=" + TMDB_API_KEY)
+  popular_tv_shows_result = popular_tv_shows_request.json()
+  popular_tv_shows = popular_tv_shows_result['results']
+
+  # get popular movies
+  popular_movies_request = requests.get("https://api.themoviedb.org/3/tv/popular_tv?api_key=" + TMDB_API_KEY)
+  popular_movies_result = popular_tv_shows_request.json()
+  popular_movies = popular_movies_result['results']
+
+
+  # get top rated tv shows
   top_rated_tv_request = requests.get("https://api.themoviedb.org/3/tv/top_rated?api_key=" + TMDB_API_KEY)
   top_rated_tv_result = top_rated_tv_request.json()
   top_rated_tv = top_rated_tv_result['results']
+   
+  # get top rated movies
+  top_rated_movie_request = requests.get("https://api.themoviedb.org/3/movie/top_rated?api_key=" + TMDB_API_KEY)
+  top_rated_movie_result = top_rated_tv_request.json()
+  top_rated_movie = top_rated_tv_result['results']
 
-  return render(request,'all-movies-tvs.html', {'now_playing_movies': now_playing_movies,})
+  # get upcoming movies
+  upcoming_movie_request = requests.get("https://api.themoviedb.org/3/movie/upcoming_movie?api_key=" + TMDB_API_KEY)
+  upcoming_movie_results = upcoming_movie_request.json()
+  upcoming_movies = upcoming_movie_results['results']
+
+  return render(request,'all-movies-tvs.html', {'now_playing_movies': now_playing_movies, 'top_rated_movie': top_rated_movie, 'top_rated_tv':top_rated_tv, 'upcoming_movies': upcoming_movies, 'popular_movies':popular_movies})
+
+  
 
