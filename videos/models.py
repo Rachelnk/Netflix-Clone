e2c68f6cdc96aad09c30b2059281415174292385
carol-profile <carolgitonga93@gmail.com>
2022-06-02 05:26:07 +0300

    add form

--- videos/models.py
+++ videos/models.py
@@ -1,7 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class UserRegistration(models.Model):
     name = models.CharField(max_length = 30)
     email = models.EmailField()
     profile_pic = models.ImageField(upload_to = 'profile/')
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='profile_images') 
+    bio = models.TextField()
+
+    def __str__(self):
+        return {self.user.username}
+
+
