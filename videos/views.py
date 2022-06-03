5124ff9ef2baf7c4d777be9c858a4072b808cbcd
Mathenge-Alex <arxmaina@gmail.com>
2022-06-03 09:29:12 +0300

    Feat: Add videos list view

--- videos/views.py
+++ videos/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
 
 # Create your views here.
+
+class VideosListView(LoginRequiredMixin, ListView):
+    model = ''
+    context_object_name = 'videos_home'
+    template_name: str = 'videos/videos_home.html'  # Can include it or not if the standard naming is followed
+    login_url = "/admin"
+
+    # def get_queryset(self):
+    #     return self.request.user.objects.all()
