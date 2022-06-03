029984245b7375749128d12d1c97449cbeafd989
maureen-james <manjax97@gmail.com>
2022-06-03 08:56:45 +0300

    feat:add details template

--- videos/models.py
+++ videos/models.py
@@ -5,3 +5,12 @@ class UserRegistration(models.Model):
     name = models.CharField(max_length = 30)
     email = models.EmailField()
     profile_pic = models.ImageField(upload_to = 'profile/')
+
+class Movie(models.Model):
+    title:models.CharField(max_length=225)
+    description:models.TextField() 
+    
+       
+class Video(models.Model):
+    title:models.CharField(max_length=225)
+    description:models.TextField() 
